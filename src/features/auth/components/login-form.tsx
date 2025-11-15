5d76b7e2f4c554742047d71cf87df2f43d9f8915
Urvi Sharma <100131855+Ur456vi@users.noreply.github.com>
2025-11-15 22:51:45 +0530

    login ui changes

---
+++ src/features/auth/components/login-form.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import {zodResolver} from "@hookform/resolvers/zod";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { toast} from "sonner";
+import {email, z} from "zod";
+import { Button } from "@/components/ui/button";
+import {
+ Card,
+ CardContent,
+ CardDescription,
+ CardHeader,
+ CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,  
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+
+
+const loginSchema = z.object({
+  email: z.email("Please enter a valid email address"),
+  password: z.string().min(1,"Password is required"),
+});
