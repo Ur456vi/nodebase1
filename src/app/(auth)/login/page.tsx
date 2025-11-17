f969180df87570a1ebfa1c9db585b408f598d216
Urvi Sharma <100131855+Ur456vi@users.noreply.github.com>
2025-11-17 15:15:49 +0530

    auth changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,11 @@
-const Page = () => {
+import { LoginForm } from "@/features/auth/components/login-form";
+import { requireUnauth } from "@/lib/auth-utils";
+
+const Page = async() => {
+  await requireUnauth();
   return(
     <div>
-       Login 
+     <LoginForm/>   
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
-import { getQueryClient,trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+"use client";
 
-const Page = async () => {
-  const queryClient = getQueryClient();
-  
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+
+const Page = async() => {
+  await requireAuth();
+
+  const data = await caller.getUsers();
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>loading...</p>}>
-
-        </Suspense>
-        <Client />
-      </HydrationBoundary>
+      protected server component
+      {JSON.stringify(data)}
     </div>
   );
 };
