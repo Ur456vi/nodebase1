b66d87bff1ff4a312dad513be9f88838a85101f2
Urvi Sharma <100131855+Ur456vi@users.noreply.github.com>
2025-11-26 18:30:01 +0530

    ai provider changes

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,21 @@
 import { z } from 'zod';
-import { createTRPCRouter, protectedProcedure } from '../init';
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 import { resolve } from 'path';
 import { inngest } from '@/inngest/client';
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
+
+
 export const appRouter = createTRPCRouter({
+
+  testAi: baseProcedure.mutation(async()=>{
+    await inngest.send({
+      name: "execute/ai"
+    });
+    return { success: true, message: "Job queued"};
+  }),
+
   getWorkflows: protectedProcedure
     .query(({ctx}) => {
       return prisma.workflow.findMany();
