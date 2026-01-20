196289ee07afb816c015ffe3600fe7a89ce1ae98
Urvi Sharma <100131855+Ur456vi@users.noreply.github.com>
2026-01-20 19:29:08 +0530

    payment changes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
+import { checkout, polar, portal} from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
-
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma,{
@@ -11,4 +12,23 @@ export const auth = betterAuth({
     enabled:true,
     autoSignIn:true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "bf9db8ce-bd69-42d2-90b4-1f7884f1dc1a",
+              slug: "pro",
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal()
+      ]
+    })
+  ]
 });
